d8c09fe59e4178f623b061b39678eb47a5755ea4
Emin Hasanov <emin.hasanov.90@outlook.com>
2022-10-18 11:09:21 +0400

    Add project files.

---
+++ CarWebApplication/Pages/Cars/Delete.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CarWebApplication.Data;
+
+namespace CarWebApplication.Pages.Cars
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly CarWebApplicationDbContext _context;
+
+        public DeleteModel(CarWebApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Car Car { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Cars == null)
+            {
+                return NotFound();
+            }
+
+            var car = await _context.Cars.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            Car = car;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Cars == null)
+            {
+                return NotFound();
+            }
+            var car = await _context.Cars.FindAsync(id);
+
+            if (car != null)
+            {
+                Car = car;
+                _context.Cars.Remove(Car);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ CarWebApplication/Pages/Cars/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CarWebApplication.Data;
+
+namespace CarWebApplication.Pages.Cars
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CarWebApplication.Data.CarWebApplicationDbContext _context;
+
+        public IndexModel(CarWebApplication.Data.CarWebApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Car> Cars { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Cars != null)
+            {
+                Cars = await _context.Cars.ToListAsync();
+            }
+        }
+    }
+}
